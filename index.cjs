48edb23b0506b9df07880cb67b930ef8b6269647
Alberto Florez <florez_alberto@hotmail.com>
2024-03-06 01:15:33 +0100

    inline test4

--- index.cjs
+++ index.cjs
@@ -1,24 +1,8 @@
-require('dotenv').config({ path: '.env' });
-const { Telegraf } = require("telegraf");
-
 
 const express = require('express');
 const app = express();
 const port = 3300;
 const path = require('path');
-
-const TOKEN = process.env.API_TOKEN; // Production bot
-
-const bot = new Telegraf(TOKEN);
-
-const web_link = "miniapp.spore.ws";
-bot.start((ctx) =>
-  ctx.reply("Welcome :)))))", {
-    reply_markup: {
-      keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
-    },
-  })
-);
 
 
 app.use(express.static('build'));
